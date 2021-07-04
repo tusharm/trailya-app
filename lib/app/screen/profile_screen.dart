24ed5f87fd1c1bfea1dc29e688506e21aea06244
Tushar Madhukar <tushar.madhukar@gmail.com>
2021-07-05 06:37:09 +1000

    Add details to the Profile screen

--- lib/app/screen/profile_screen.dart
+++ lib/app/screen/profile_screen.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:trailya/model/config.dart';
+import 'package:trailya/services/auth.dart';
 
 class ProfileScreen extends StatelessWidget {
   ProfileScreen({required this.config});
@@ -8,6 +11,9 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final auth = Provider.of<FirebaseAuthentication>(context, listen: false);
+    final user = auth.currentUser!;
+
     return SingleChildScrollView(
       padding: EdgeInsets.all(12),
       child: Center(
@@ -15,28 +21,60 @@ class ProfileScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Card(
-              shape: RoundedRectangleBorder(),
-              child: Column(
-                children: [
-                  RadioListTile<Location>(
-                    title: Text(Location.NSW.asString()),
-                    value: Location.NSW,
-                    groupValue: config.location,
-                    onChanged: _onChanged,
-                  ),
-                  RadioListTile<Location>(
-                    title: Text(Location.VIC.asString()),
-                    value: Location.VIC,
-                    groupValue: config.location,
-                    onChanged: _onChanged,
-                  ),
-                ],
+            _buildAvatar(user),
+            SizedBox(height: 10),
+            if (user.email != null)
+              Text(
+                user.email!,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black, fontSize: 15),
               ),
-            )
+            SizedBox(height: 20),
+            _buildStateSelector()
           ],
         ),
       ),
+    );
+  }
+
+  Card _buildStateSelector() {
+    return Card(
+      shape: RoundedRectangleBorder(),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Text(
+              'Choose state',
+              style: TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 8),
+            RadioListTile<Location>(
+              title: Text(Location.NSW.asString()),
+              value: Location.NSW,
+              groupValue: config.location,
+              onChanged: _onChanged,
+            ),
+            RadioListTile<Location>(
+              title: Text(Location.VIC.asString()),
+              value: Location.VIC,
+              groupValue: config.location,
+              onChanged: _onChanged,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  CircleAvatar _buildAvatar(User user) {
+    return CircleAvatar(
+      radius: 50,
+      backgroundImage:
+          user.photoURL != null ? NetworkImage(user.photoURL!) : null,
+      child: user.photoURL == null ? Icon(Icons.camera_alt, size: 50) : null,
     );
   }
 
