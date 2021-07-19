0d51cc818b6848e87f40eabd229b6ec48600e369
Tushar Madhukar <tushar.madhukar@gmail.com>
2021-07-20 07:17:01 +1000

    Updated map markers

--- lib/utils/assets.dart
+++ lib/utils/assets.dart
@@ -9,22 +9,40 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class Assets {
   static BitmapDescriptor? redMarkerIcon;
   static BitmapDescriptor? blueMarkerIcon;
+  static BitmapDescriptor? orangeMarkerIcon;
 
   static Future<void> init() async {
-    redMarkerIcon = await _getMarkerBitmap(75, Colors.red);
-    blueMarkerIcon = await _getMarkerBitmap(75, Colors.blueAccent);
+    redMarkerIcon = await _getMarkerBitmap(75, Colors.red, line: true);
+    blueMarkerIcon = await _getMarkerBitmap(75, Colors.green);
+    orangeMarkerIcon = await _getMarkerBitmap(75, Colors.orange);
   }
 
   static Future<BitmapDescriptor> _getMarkerBitmap(
-      int size, Color color) async {
+    int size,
+    Color color, {
+    bool line = false,
+  }) async {
     final pictureRecorder = PictureRecorder();
     final canvas = Canvas(pictureRecorder);
     final paint1 = Paint()..color = color;
     final paint2 = Paint()..color = Colors.white;
 
-    canvas.drawCircle(Offset(size / 2, size / 2), size / 2.0, paint1);
-    canvas.drawCircle(Offset(size / 2, size / 2), size / 2.2, paint2);
-    canvas.drawCircle(Offset(size / 2, size / 2), size / 2.8, paint1);
+    final center = Offset(size / 2, size / 2);
+    canvas.drawCircle(center, size / 2.0, paint1);
+    canvas.drawCircle(center, size / 2.2, paint2);
+    canvas.drawCircle(center, size / 2.8, paint1);
+    if (line) {
+      canvas.drawRRect(
+        RRect.fromLTRBR(
+          center.dx - size / 4.5,
+          center.dy + size / 12.0,
+          center.dx + size / 4.5,
+          center.dy - size / 12.0,
+          Radius.circular(2.0),
+        ),
+        paint2,
+      );
+    }
 
     final img = await pictureRecorder.endRecording().toImage(size, size);
     return _imageToBitmap(img);
